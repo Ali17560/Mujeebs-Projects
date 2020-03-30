cd9c45eb6e71963fe85e83829284cc8b733c90b9
Ali17560 <55414347+Ali17560@users.noreply.github.com>
2020-03-30 01:54:03 -0400

    GPA

---
+++ Calculate Your GPA/src/GPAAverage.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class   {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		double A=4.0;
+		double AMinus=3.7;
+		double BPlus=3.33;
+		double B=3.0;
+		double BMinus=2.7;
+		double CPlus=2.3;
+		double C=2.0;
+		double CMinus=1.7;
+		double DPlus=1.3;
+		double D=1.0;
+		double DMinus=0.70;
+		double number=0;
+
+		System.out.println("Enter the course information spearated by commas(ttile, credit hours, lettter grade) or type Q to quit: ");
+		double a=in.nextDouble();
+		
+		if(a==A)
+		{
+			System.out.print(number=4.0);
+		}
+		else if(a==AMinus)
+		{
+			System.out.print(number=3.7);
+		}
+		else if(a==BPlus)
+		{
+			number=3.33;
+		}
+		else if(a==B)
+		{
+			number=3.0;
+		}
+		else if(a==BMinus)
+		{
+			number=2.7;
+		}
+			}
+		else if(a==CPlus)
+		{
+			number=2.3;
+		}
+		else if(a==C)
+		{
+			number=2.0;
+		}
+		else if(a==CMinus)
+		{
+			number=1.7;
+		}
+		else if(a==DPlus)
+		{
+			number=1.3;
+		}
+		else  if(a==D)
+		{
+			number=1;
+		}
+		else if(a==DMinus)
+		{
+			number=0.70;
+		}
+	
+			
+		
+	
+}
