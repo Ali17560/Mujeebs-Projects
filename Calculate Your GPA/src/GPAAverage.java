67862dfb5c9a8f6a402f77e920be0546ed441138
Ali17560 <55414347+Ali17560@users.noreply.github.com>
2020-03-30 23:40:06 -0400

    real gpa

--- Calculate Your GPA/src/GPAAverage.java
+++ Calculate Your GPA/src/GPAAverage.java
@@ -1,72 +1,123 @@
 import java.util.Scanner;
-public class   {
+public class GPAAverage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in=new Scanner(System.in);
-		double A=4.0;
-		double AMinus=3.7;
-		double BPlus=3.33;
-		double B=3.0;
-		double BMinus=2.7;
-		double CPlus=2.3;
-		double C=2.0;
-		double CMinus=1.7;
-		double DPlus=1.3;
-		double D=1.0;
-		double DMinus=0.70;
-		double number=0;
 
-		System.out.println("Enter the course information spearated by commas(ttile, credit hours, lettter grade) or type Q to quit: ");
-		double a=in.nextDouble();
-		
-		if(a==A)
-		{
-			System.out.print(number=4.0);
-		}
-		else if(a==AMinus)
-		{
-			System.out.print(number=3.7);
-		}
-		else if(a==BPlus)
-		{
-			number=3.33;
-		}
-		else if(a==B)
-		{
-			number=3.0;
-		}
-		else if(a==BMinus)
-		{
-			number=2.7;
-		}
-			}
-		else if(a==CPlus)
-		{
-			number=2.3;
-		}
-		else if(a==C)
-		{
-			number=2.0;
-		}
-		else if(a==CMinus)
-		{
-			number=1.7;
-		}
-		else if(a==DPlus)
-		{
-			number=1.3;
-		}
-		else  if(a==D)
-		{
-			number=1;
-		}
-		else if(a==DMinus)
-		{
-			number=0.70;
-		}
-	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner in=new Scanner(System.in);
+			int sum=0;
+			int clas=0;
+			int  value=0;
+			double letter=0;
+			double gradeSum=0;
+			double grade=0;
+			double GPA=0;
 			
+			while(sum<10) {
+			
+			System.out.println("Enter course information separated by commas (title, credit hours, letter grade) or type Q to quit: ");
+			String a=in.nextLine();
+			
+			if(a.equals("q")||a.equals("Q")){
+				System.out.println("Quit the program");
+				break;}
+			
+			String [] parameters=a.split(",");
+			String credits=parameters[1];	
+			String letterGr=parameters[2];
+			
+			if(letterGr.equals("A"))
+			{letterGr="4.0";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("A-"))
+			{letterGr="3.7";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("B+"))
+			{letterGr="3.33";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("B"))
+			{letterGr="3.0";
+		    value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("B-"))
+			{letterGr="2.7";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("C+"))
+			{letterGr="2.3";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("C"))
+			{letterGr="2.0";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("C-"))
+			{letterGr="1.7";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("D+"))
+			{letterGr="1.3";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("D"))
+			{letterGr="1.0";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("D-"))
+			{letterGr=".70";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("F")||letterGr.equals("WF"))
+			{letterGr="0";
+			value=Integer.parseInt(credits);
+			letter=Double.parseDouble(letterGr);}
+			
+			else if(letterGr.equals("P")||letterGr.equals("NP")||letterGr.equals("I")||letterGr.equals("W")) 
+			{letterGr=null;
+			value=Integer.parseInt(credits);}
+			
+			else
+			{System.out.println("Invalid grade.");
+			letterGr=null;
+			value=0;}
+			
+			clas++;
+			sum+=value;
+			System.out.println(clas+ " classes ");
+			System.out.println(sum+ " credits ");
+			
+			grade=letter*value;	
+			
+			if(sum>10)
+			{System.out.println("\nYou can only enter a maximum of 10 credits");
+			clas--;
+			sum-=value;
+			System.out.println(clas+ " classes ");
+			System.out.println(sum+ " credits");
+			grade=0;}
 		
-	
-}
+			gradeSum+=grade;
+			System.out.printf("Grade Points: %.2f\n\n",gradeSum );
+			
+		}	
+			GPA=gradeSum/10;
+			System.out.printf("\nYour GPA for the semester is a %.2f",GPA);
+		}
+		{
+
+		}
+		}
